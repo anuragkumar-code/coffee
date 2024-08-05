2878415a3fc71b416bd70311f2beb5c0918689b8
anuragkumar-code <kumar.mairwa@gmail.com>
2024-08-05 20:28:32 +0530

    completed home dashboard

--- partials/header.php
+++ partials/header.php
@@ -22,6 +22,9 @@
 
 		<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 		<script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap4.min.js"></script>
+
+		<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 	</head>
 
 	<body class="main-body app sidebar-mini ltr">
